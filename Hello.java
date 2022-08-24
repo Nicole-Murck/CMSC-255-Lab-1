258188e6ebcb64fb6eeac54df288774b07c9ad90
Nicole-Murck <112000207+Nicole-Murck@users.noreply.github.com>
2022-08-24 11:27:30 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*
+* Hello Program
+*
+* Nicole Murck
+* 8/24/2022
+* CMSC 255 004
+*/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Nicole Murck.");
+		System.out.println("Hello Program.");
+		System.out.println("This program is designed to print information about itself and the programmer, a student in Intro to Programming.");
+		System.out.println("The 24th of August, 2022.");
+		System.out.println();
+		
+	}
+}
